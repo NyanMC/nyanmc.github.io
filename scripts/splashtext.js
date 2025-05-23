1d4ec7302cc7b5add25456f762cbf0f3bd5ad5a5
ChromaNyan <nyancatz593@gmail.com>
2025-05-23 02:31:09 -0500

    summary (required)

--- scripts/splashtext.js
+++ scripts/splashtext.js
@@ -1,4 +1,4 @@
-var splashes = [
+const splashes = [
     "one of the space cats ever",
     "humanity is for chumps",
     "your local cosmic feline",
@@ -25,17 +25,58 @@ var splashes = [
     "JAR transformation complete",
     "formerly immensely gay",
     "was once called a weasel. that was a compliment",
-    "\"One day, somewhere in the future, my work will be quoted!\" -Minecraft splash text #313"
+    "\"One day, somewhere in the future, my work will be quoted!\" -Minecraft splash text #313",
+    "check it out, i'm in the house, like carpet",
+    "have you met umbranyan yet"
 ]
 
+const specialDays = [
+    { // birthday
+        "month": 3,
+        "day": 24
+    },
+    { // proper creation of the void
+        "month": 5,
+        "day": 24
+    },
+    { // listed creation date of chromatic arsenal
+        "month": 8,
+        "day": 6
+    },
+    { // date my gf became my gf
+        "month": 11,
+        "day": 29
+    }
+]
+
+function isTodaySpecial() {
+    let curDate = new Date();
+
+    for (let i = 0; i < specialDays.length; i++) {
+        let element = specialDays[i];
+        if (curDate.getMonth() + 1 === element.month && curDate.getDate() === element.day) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 function generateSplash() {
-    var randomSplash = splashes[Math.floor(Math.random() * splashes.length)];
+    if (isTodaySpecial()) {
+        $("#splashtext").text("today is a special day...");
+        return;
+    }
+
     // 3ds detection hopefully
     if ((navigator.platform.indexOf("Nintendo 3DS") > -1) || (screen.pixelDepth == 16 && navigator.platform == "iPhone")) {
-        $("#splashtext").text("holy fucking shit is that the Nintendo 3DS Internet Browser");
-    } else {
-        $("#splashtext").text(randomSplash);
+        $("#splashtext").text("is that the Nintendo 3DS Internet Browser? what an honor");
+        return;
     }
+
+    let randomSplash = splashes[Math.floor(Math.random() * splashes.length)];
+
+    $("#splashtext").text(randomSplash);
 }
 
 $(document).ready(function() {
